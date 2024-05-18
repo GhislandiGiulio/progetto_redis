ca63facbc6aaaf20a4a026b8595d56f976e8ec51
Hypnos999 <flama2204@gmail.com>
2024-05-18 19:07:45 +0200

    Sistemato uso della variable x utente attivo, aggiunta logica dell'output della funzione principale

--- redis_chat.py
+++ redis_chat.py
@@ -1,73 +1,71 @@
 import redis
 import os
 
-# inizializzazione utente logged-in. Si potrebbe aggiungere una cache per memorizzarlo anche se si esce dall'esecuzione.
-active_user = None
 
-r = redis.Redis(
-                    host="localhost",
-                    port=6379,
-                    db=0,
-                    decode_responses=True
-                )
-
-# wrapper per le differenti schermate
+## wrapper per le differenti schermate
 def schermata(funzione):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         if os.name == 'nt':
-            # Per Windows
+            ## Per Windows
             os.system('cls')
         else:
-            # Per Unix/Linux/macOS
+            ## Per Unix/Linux/macOS
             os.system('clear')
 
-        print("Utente attivo: ", active_user)
-        funzione()
+        return funzione(*args, **kwargs)
 
     return wrapper
 
 @schermata
-def menu_iniziale():
-
+def menu_iniziale(r: redis.Redis, user: str) -> bool|str|None:
+    '''Ritorna True se l'utente vuole uscire dal programma
+Ritorna False se l'utente ha sbagliato la scelta o è uscito da una funzione
+Ritorna None o str in tutti gli altri casi, è il valore dell'utente attivo aggiornato.'''
+    print("Benvenuto", user if user != None else "anonimo")
+    
     try:
-        scelta = int(input("Scegli un'opzione:\n1- Registrazione \n2- Login \n3- Logout\n4- Chat\n5- Esci dal programma\n"))
+        scelta = int(input("\n:Scegli un'opzione:\n1- Registrazione \n2- Login \n3- Logout\n4- Chat\n5- Esci dal programma\n"))
     except ValueError:
-        return
+        return False
 
     match scelta:
         case 1:
-            registrazione()
+            user = registrazione(r)
         case 2:
-            login()
+            user = login(r)
         case 3:
-            logout()
+            user = logout(r)
+            # return True ## fate sapere se secondo voi dopo il logout dovrebbe anche uscire dal programma o no?
         case 4:
-            ...
-        case 5:
-            exit(0)
-        case _:
+            ## TODO: implementare la chat
             pass
+        case 5:
+            return True
+        case _:
+            return False
+    
+    return user
 
 @schermata
-def registrazione():
+def registrazione(r: redis.Redis):
 
     print("Se vuoi uscire in qualunque momento, inserisci 'q'")
 
-    # ciclo pe rinserimento del numero di telefono
+    ## ciclo pe rinserimento del numero di telefono
     while True:
         nome_utente = input("Inserisci il tuo nuovo nome utente: ")
 
-        # verifica se l'utente vuole uscire
+        ## verifica se l'utente vuole uscire
         if nome_utente == "q":
-            return
+            return False
 
-        # verifica sulla lunghezza minima e massima del nome utente
+        ## verifica sulla lunghezza minima e massima del nome utente
         lunghezza_nomeutente = len(nome_utente)
         if lunghezza_nomeutente < 3 or lunghezza_nomeutente > 20:
             print("Il nome utente deve avere una lunghezza compresa tra 3 e 20 (compresi)")
             continue
 
-        # verifica dell'esistenza pregressa del nome utente
+        ## verifica dell'esistenza pregressa del nome utente
         output = r.hget("users", nome_utente)
         if output == None:
             break
@@ -75,91 +73,104 @@ def registrazione():
         print("Nome utente già inserito.")
 
     
-    # ciclo per inserimento corretto del numero di telefono
+    ## ciclo per inserimento corretto del numero di telefono
     while True:
-        # ciclo per verifica della formattazione del numero di telefono    
+        ## ciclo per verifica della formattazione del numero di telefono    
         while True:
             numero_telefono = input("Inserisci il tuo numero di telefono: +")
-
-            # verifica se l'utente vuole uscire
+            
+            ## verifica se l'utente vuole uscire
             if numero_telefono == "q":
-                return
+                return False
+
+            ## rimozione degli spazi
+            numero_telefono = numero_telefono.replace(" ", "")
 
             try:
-                
-                # verifica della lunghezza del numero
-                lunghezza_numero = len(numero_telefono)    
+                ## verifica della lunghezza del numero
+                lunghezza_numero = len(numero_telefono)
                 if lunghezza_numero < 12 or lunghezza_numero > 13:
                     print("Il numero di telefono deve avere 12/13 cifre (incluso il prefisso).")
                     continue 
 
-                # tentativo di casting a numero intero per verifica presenza caratteri non numerici
+                ## tentativo di casting a numero intero per verifica presenza caratteri non numerici
                 numero_telefono = int(numero_telefono)
                 break
             
             except ValueError:
                 print("Il numero di telefono contiene simboli oltre ai numeri.")
 
-        # verifica dell'esistenza pregressa del numero di telefono
+        ## verifica dell'esistenza pregressa del numero di telefono
         output = r.hget("phone_number", numero_telefono)
         if output == None:
             break
 
         print("Numero già registrato.")
-
     
-    # input della password
+    ## input della password
     password = input("Inserisci la tua nuova password: ")
     
-    # verifica se l'utente vuole uscire
+    ## verifica se l'utente vuole uscire
     if password == "q":
-        return
+        return False
     
-    # aggiunta delle chiavi all'hashmap Redis
+    ## aggiunta delle chiavi all'hashmap Redis
     r.hset("users", nome_utente, password)
     r.hset("phone_number", numero_telefono, nome_utente)
 
-@schermata
-def login():
+    return nome_utente
 
+@schermata
+def login(r: redis.Redis):
     print("Se vuoi uscire in qualunque momento, inserisci 'q'")
 
-    # inserimento del nome utente
+    ## inserimento del nome utente
     nome_utente = input("Inserisci il tuo nome utente: ")
 
-    # esci se l'utente inserisce "q"
+    ## esci se l'utente inserisce "q"
     if nome_utente == "q":
-        return
+        return False
 
-    # inserimento della password
+    ## inserimento della password
     password = input("Inserisci la password: ")
 
-    # esci se l'utente inserisce "q"
+    ## esci se l'utente inserisce "q"
     if password == "q":
-        return
+        return False
     
-    # verifica della correttezza della password / esistenza dell'utente inserito
+    ## verifica della correttezza della password / esistenza dell'utente inserito
     output = r.hget("users", nome_utente)
 
     if output == password and output != None :
-        global active_user
-        active_user = nome_utente
+        return nome_utente
 
 @schermata
-def logout():
-
-    global active_user
-
-    if active_user != None:
-
+def logout(user: str|None):
+    if user != None:
         decisione = input("Sei sicuro di voler effettuare il logout?\ny=Sì\nn=No\n")
 
         if decisione == "y":
-            active_user = None
+            return None
+        else:
+            return user
 
 
 if __name__ == "__main__":
+    ## inizializzazione utente logged-in. Si potrebbe aggiungere una cache per memorizzarlo anche se si esce dall'esecuzione.
+    user = None
+    r = redis.Redis(
+        host="localhost",
+        port=8765,
+        db=0,
+        decode_responses=True
+    )
 
     while True:
-        menu_iniziale()
-    
+        output = menu_iniziale(r, user)
+        if type(output) in [str, type(None)]:
+            user = output
+            continue
+        
+        if output:
+            print('\nTerminando l\'esecuzione.')
+            break
